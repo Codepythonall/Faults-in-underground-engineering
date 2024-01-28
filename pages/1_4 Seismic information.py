b85bbd30a35a48231b996f32c10913aa5c40a928
LifengXX <147348754+LifengXX@users.noreply.github.com>
2024-01-28 16:48:21 -0700

    Update 1_4 Seismic information.py

--- pages/1_4 Seismic information.py
+++ pages/1_4 Seismic information.py
@@ -71,6 +71,496 @@ sys.path.append(os.getcwd())
 from pathlib import Path
 st.set_page_config(layout="wide")
 
-a=pd.read_csv(Path(__file__).parent / "Data/GEoREST_Fault.csv")
 
+
+b=pd.read_csv(Path(__file__).parent / "Data/Seismic information.csv")
+# Throught the selectbox to dreaw the echart
+optionA1 = st.selectbox(
+    'Which item do you like best?',
+    b.columns.tolist())
+r1=b.groupby(optionA1).size()
+
+options1 = {
+    "color":'#ff4060',
+    "tooltip": {
+  "trigger": 'axis',
+  "axisPointer": {
+    "type": 'shadow'
+  }
+},
+    "xAxis": {
+        "type": "category",
+        "data": r1.index.tolist(),
+        "axisTick": {"alignWithLabel": True},
+    },
+    "yAxis": {"type": "value"},
+    "series": [
+        {"data": r1.values.tolist(), "type": "bar"}
+    ],
+}
+st_echarts(options=options1)
+
+
+
+
+
+
+
+
+a=pd.read_csv(Path(__file__).parent / "Data/GEoREST_Fault.csv")
+fig1 = px.scatter_mapbox(a, 
+                        lat='latitude', 
+                        lon='longitude', 
+                        color='moment_max',
+                        color_continuous_scale=[                
+                            '#800080',
+                            '#0000FF',
+                            '#00FFFF',
+                            '#008000',
+                            '#FFFF00',
+                            '#FFA500',
+                            '#FF0000',
+                        ],
+                        range_color=(-6, 8),
+                        mapbox_style="carto-positron",
+                        opacity=0.5,
+                        labels={'diff_percentage':'Difference Percentage'},
+                        center={"lat": 53, "lon": -113},
+                        zoom=0.1)
+
+st.plotly_chart(fig1)
+st.write ("Figure 1. moment_max")
+
+
+fig2 = px.scatter_mapbox(a, 
+                        lat='latitude', 
+                        lon='longitude', 
+                        color='moment_max_type',
+                        color_continuous_scale=[                
+                            '#800080',
+                            '#0000FF',
+                            '#00FFFF',
+                            '#008000',
+                            '#FFFF00',
+                            '#FFA500',
+                            '#FF0000',
+                        ],
+                        range_color=(0, 12),
+                        mapbox_style="carto-positron",
+                        opacity=0.5,
+                        labels={'diff_percentage':'Difference Percentage'},
+                        center={"lat": 53, "lon": -113},
+                        zoom=0.1)
+
+st.plotly_chart(fig2)
+st.write ("Figure 2. moment_max_type")
+
+
+fig3 = px.scatter_mapbox(a, 
+                        lat='latitude', 
+                        lon='longitude', 
+                        color='moment_max_depth_min',
+                        color_continuous_scale=[                
+                            '#800080',
+                            '#0000FF',
+                            '#00FFFF',
+                            '#008000',
+                            '#FFFF00',
+                            '#FFA500',
+                            '#FF0000',
+                        ],
+                        range_color=(0, 6000),
+                        mapbox_style="carto-positron",
+                        opacity=0.5,
+                        labels={'diff_percentage':'Difference Percentage'},
+                        center={"lat": 53, "lon": -113},
+                        zoom=0.1)
+
+st.plotly_chart(fig3)
+st.write ("Figure 3. moment_max_depth_min")
+
+
+fig4 = px.scatter_mapbox(a, 
+                        lat='latitude', 
+                        lon='longitude', 
+                        color='moment_max_depth_max',
+                        color_continuous_scale=[                
+                            '#800080',
+                            '#0000FF',
+                            '#00FFFF',
+                            '#008000',
+                            '#FFFF00',
+                            '#FFA500',
+                            '#FF0000',
+                        ],
+                        range_color=(0, 14000),
+                        mapbox_style="carto-positron",
+                        opacity=0.5,
+                        labels={'diff_percentage':'Difference Percentage'},
+                        center={"lat": 53, "lon": -113},
+                        zoom=0.1)
+
+st.plotly_chart(fig4)
+st.write ("Figure 4. moment_max_depth_max")
+
+
+fig5 = px.scatter_mapbox(a, 
+                        lat='latitude', 
+                        lon='longitude', 
+                        color='moment_distance',
+                        color_continuous_scale=[                
+                            '#800080',
+                            '#0000FF',
+                            '#00FFFF',
+                            '#008000',
+                            '#FFFF00',
+                            '#FFA500',
+                            '#FF0000',
+                        ],
+                        range_color=(0, 20000),
+                        mapbox_style="carto-positron",
+                        opacity=0.5,
+                        labels={'diff_percentage':'Difference Percentage'},
+                        center={"lat": 53, "lon": -113},
+                        zoom=0.1)
+
+st.plotly_chart(fig5)
+st.write ("Figure 5. moment_distance")
+
+
+fig6 = px.scatter_mapbox(a, 
+                        lat='latitude', 
+                        lon='longitude', 
+                        color='moment_date',
+                        color_continuous_scale=[                
+                            '#800080',
+                            '#0000FF',
+                            '#00FFFF',
+                            '#008000',
+                            '#FFFF00',
+                            '#FFA500',
+                            '#FF0000',
+                        ],
+                        range_color=(0, 12),
+                        mapbox_style="carto-positron",
+                        opacity=0.5,
+                        labels={'diff_percentage':'Difference Percentage'},
+                        center={"lat": 53, "lon": -113},
+                        zoom=0.1)
+
+st.plotly_chart(fig6)
+st.write ("Figure 6. moment_date")
+
+
+fig7 = px.scatter_mapbox(a, 
+                        lat='latitude', 
+                        lon='longitude', 
+                        color='reason',
+                        color_continuous_scale=[                
+                            '#800080',
+                            '#0000FF',
+                            '#00FFFF',
+                            '#008000',
+                            '#FFFF00',
+                            '#FFA500',
+                            '#FF0000',
+                        ],
+                        range_color=(0, 12),
+                        mapbox_style="carto-positron",
+                        opacity=0.5,
+                        labels={'diff_percentage':'Difference Percentage'},
+                        center={"lat": 53, "lon": -113},
+                        zoom=0.1)
+
+st.plotly_chart(fig7)
+st.write ("Figure 7. reason")
+
+
+fig8 = px.scatter_mapbox(a, 
+                        lat='latitude', 
+                        lon='longitude', 
+                        color='comment',
+                        color_continuous_scale=[                
+                            '#800080',
+                            '#0000FF',
+                            '#00FFFF',
+                            '#008000',
+                            '#FFFF00',
+                            '#FFA500',
+                            '#FF0000',
+                        ],
+                        range_color=(0, 12),
+                        mapbox_style="carto-positron",
+                        opacity=0.5,
+                        labels={'diff_percentage':'Difference Percentage'},
+                        center={"lat": 53, "lon": -113},
+                        zoom=0.1)
+
+st.plotly_chart(fig8)
+st.write ("Figure 8. comment")
+
+
+fig9 = px.scatter_mapbox(a, 
+                        lat='latitude', 
+                        lon='longitude', 
+                        color='seism_onset',
+                        color_continuous_scale=[                
+                            '#800080',
+                            '#0000FF',
+                            '#00FFFF',
+                            '#008000',
+                            '#FFFF00',
+                            '#FFA500',
+                            '#FF0000',
+                        ],
+                        range_color=(0, 12),
+                        mapbox_style="carto-positron",
+                        opacity=0.5,
+                        labels={'diff_percentage':'Difference Percentage'},
+                        center={"lat": 53, "lon": -113},
+                        zoom=0.1)
+
+st.plotly_chart(fig9)
+st.write ("Figure 9. seism_onset")
+
+
+fig10 = px.scatter_mapbox(a, 
+                        lat='latitude', 
+                        lon='longitude', 
+                        color='seism_time_shift_onset',
+                        color_continuous_scale=[                
+                            '#800080',
+                            '#0000FF',
+                            '#00FFFF',
+                            '#008000',
+                            '#FFFF00',
+                            '#FFA500',
+                            '#FF0000',
+                        ],
+                        range_color=(0, 12),
+                        mapbox_style="carto-positron",
+                        opacity=0.5,
+                        labels={'diff_percentage':'Difference Percentage'},
+                        center={"lat": 53, "lon": -113},
+                        zoom=0.1)
+
+st.plotly_chart(fig10)
+st.write ("Figure 10. seism_time_shift_onset")
+
+
+
+fig11 = px.scatter_mapbox(a, 
+                        lat='latitude', 
+                        lon='longitude', 
+                        color='seism_events',
+                        color_continuous_scale=[                
+                            '#800080',
+                            '#0000FF',
+                            '#00FFFF',
+                            '#008000',
+                            '#FFFF00',
+                            '#FFA500',
+                            '#FF0000',
+                        ],
+                        range_color=(0, 12),
+                        mapbox_style="carto-positron",
+                        opacity=0.5,
+                        labels={'diff_percentage':'Difference Percentage'},
+                        center={"lat": 53, "lon": -113},
+                        zoom=0.1)
+
+st.plotly_chart(fig11)
+st.write ("Figure 11. seism_events")
+
+
+fig12 = px.scatter_mapbox(a, 
+                        lat='latitude', 
+                        lon='longitude', 
+                        color='seism_depth_min',
+                        color_continuous_scale=[                
+                            '#800080',
+                            '#0000FF',
+                            '#00FFFF',
+                            '#008000',
+                            '#FFFF00',
+                            '#FFA500',
+                            '#FF0000',
+                        ],
+                        range_color=(0, 9000),
+                        mapbox_style="carto-positron",
+                        opacity=0.5,
+                        labels={'diff_percentage':'Difference Percentage'},
+                        center={"lat": 53, "lon": -113},
+                        zoom=0.1)
+
+st.plotly_chart(fig12)
+st.write ("Figure 12. seism_depth_min")
+
+
+fig13 = px.scatter_mapbox(a, 
+                        lat='latitude', 
+                        lon='longitude', 
+                        color='seism_depth_max',
+                        color_continuous_scale=[                
+                            '#800080',
+                            '#0000FF',
+                            '#00FFFF',
+                            '#008000',
+                            '#FFFF00',
+                            '#FFA500',
+                            '#FF0000',
+                        ],
+                        range_color=(0, 20000),
+                        mapbox_style="carto-positron",
+                        opacity=0.5,
+                        labels={'diff_percentage':'Difference Percentage'},
+                        center={"lat": 53, "lon": -113},
+                        zoom=0.1)
+
+st.plotly_chart(fig13)
+st.write ("Figure 13. seism_depth_max")
+
+
+fig14 = px.scatter_mapbox(a, 
+                        lat='latitude', 
+                        lon='longitude', 
+                        color='seism_a_before',
+                        color_continuous_scale=[                
+                            '#800080',
+                            '#0000FF',
+                            '#00FFFF',
+                            '#008000',
+                            '#FFFF00',
+                            '#FFA500',
+                            '#FF0000',
+                        ],
+                        range_color=(0, 12),
+                        mapbox_style="carto-positron",
+                        opacity=0.5,
+                        labels={'diff_percentage':'Difference Percentage'},
+                        center={"lat": 53, "lon": -113},
+                        zoom=0.1)
+
+st.plotly_chart(fig14)
+st.write ("Figure 14. seism_a_before")
+
+
+fig15 = px.scatter_mapbox(a, 
+                        lat='latitude', 
+                        lon='longitude', 
+                        color='seism_b_before',
+                        color_continuous_scale=[                
+                            '#800080',
+                            '#0000FF',
+                            '#00FFFF',
+                            '#008000',
+                            '#FFFF00',
+                            '#FFA500',
+                            '#FF0000',
+                        ],
+                        range_color=(0, 12),
+                        mapbox_style="carto-positron",
+                        opacity=0.5,
+                        labels={'diff_percentage':'Difference Percentage'},
+                        center={"lat": 53, "lon": -113},
+                        zoom=0.1)
+
+st.plotly_chart(fig15)
+st.write ("Figure 15. seism_b_before")
+
+
+fig16 = px.scatter_mapbox(a, 
+                        lat='latitude', 
+                        lon='longitude', 
+                        color='seism_a_during',
+                        color_continuous_scale=[                
+                            '#800080',
+                            '#0000FF',
+                            '#00FFFF',
+                            '#008000',
+                            '#FFFF00',
+                            '#FFA500',
+                            '#FF0000',
+                        ],
+                        range_color=(0, 12),
+                        mapbox_style="carto-positron",
+                        opacity=0.5,
+                        labels={'diff_percentage':'Difference Percentage'},
+                        center={"lat": 53, "lon": -113},
+                        zoom=0.1)
+
+st.plotly_chart(fig16)
+st.write ("Figure 16. seism_a_during")
+
+
+fig17 = px.scatter_mapbox(a, 
+                        lat='latitude', 
+                        lon='longitude', 
+                        color='seism_b_during',
+                        color_continuous_scale=[                
+                            '#800080',
+                            '#0000FF',
+                            '#00FFFF',
+                            '#008000',
+                            '#FFFF00',
+                            '#FFA500',
+                            '#FF0000',
+                        ],
+                        range_color=(0, 12),
+                        mapbox_style="carto-positron",
+                        opacity=0.5,
+                        labels={'diff_percentage':'Difference Percentage'},
+                        center={"lat": 53, "lon": -113},
+                        zoom=0.1)
+
+st.plotly_chart(fig17)
+st.write ("Figure 17. seism_b_during")
+
+
+fig18 = px.scatter_mapbox(a, 
+                        lat='latitude', 
+                        lon='longitude', 
+                        color='seism_a_after',
+                        color_continuous_scale=[                
+                            '#800080',
+                            '#0000FF',
+                            '#00FFFF',
+                            '#008000',
+                            '#FFFF00',
+                            '#FFA500',
+                            '#FF0000',
+                        ],
+                        range_color=(0, 12),
+                        mapbox_style="carto-positron",
+                        opacity=0.5,
+                        labels={'diff_percentage':'Difference Percentage'},
+                        center={"lat": 53, "lon": -113},
+                        zoom=0.1)
+
+st.plotly_chart(fig18)
+st.write ("Figure 18. seism_a_after")
+
+
+fig19 = px.scatter_mapbox(a, 
+                        lat='latitude', 
+                        lon='longitude', 
+                        color='seism_b_after',
+                        color_continuous_scale=[                
+                            '#800080',
+                            '#0000FF',
+                            '#00FFFF',
+                            '#008000',
+                            '#FFFF00',
+                            '#FFA500',
+                            '#FF0000',
+                        ],
+                        range_color=(0, 12),
+                        mapbox_style="carto-positron",
+                        opacity=0.5,
+                        labels={'diff_percentage':'Difference Percentage'},
+                        center={"lat": 53, "lon": -113},
+                        zoom=0.1)
+
+st.plotly_chart(fig19)
+st.write ("Figure 19. seism_b_after")
 
